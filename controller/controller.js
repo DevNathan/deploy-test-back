09d330b9e582b64a19748808437626b84a4e97d6
devnamho0910 <devnamho0910@gmail.com>
2024-07-05 20:47:04 +0900

    auto commit test

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,7 @@ import user from '../schemas/user.js';
 
 const main = (req, res) => {
     res.set({ "Content-Type": "text/html; charset=utf-8" });
-    res.send("<h1>Welcome Express!</h1>");
+    res.send("<h1>Welcome Express! auto commit test</h1>");
 };
 
 const test = async (req, res) => {
